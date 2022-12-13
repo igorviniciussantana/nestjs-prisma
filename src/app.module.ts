e40f142b47e1cd2f894eae80a19054e73aca2b66
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-13 10:55:52 -0400

    feat: setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MailService } from './mail/mail.service';
-import { PostMarkMailService } from './mail/postmark-mail.service';
-import { SMTPMailService } from './mail/smtp-mail.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
-    {
-      provide: MailService,
-      useClass: PostMarkMailService
-    }
+    PrismaService,
   ],
 })
 export class AppModule {}
