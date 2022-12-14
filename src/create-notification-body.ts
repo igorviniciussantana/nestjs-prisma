f5a301ba0e21e565c62025c484f6f03cd6f17961
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-14 10:01:13 -0400

    feat: add validation pipe

--- src/create-notification-body.ts
+++ src/create-notification-body.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty } from "class-validator";
+
+
 export class CreateNotificationBody{
+    @IsNotEmpty()
     recipientId: string;
+
+
+
     content: string;
+
+
     category: string;
 }
