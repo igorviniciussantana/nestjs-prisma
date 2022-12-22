7b756956bbb67d3705715d0cd180c04f85ad02ee
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-22 10:35:12 -0400

    test: add content test

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,1 +1,8 @@
-test('it should be able to create a notification content')
+import { Content } from "./content"
+
+test('it should be able to create a notification content', () =>{
+    const content = new Content('Você recebeu uma solicitação de amizade')
+
+
+    expect(content).toBeTruthy()
+})
