b5bd170b1b37c9287cdaf52e5f7b2973b4f45626
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 09:44:54 -0400

    feat: add read methods to notifications entity

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -7,7 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
-  canceledAt?: Date | null,
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -51,16 +51,20 @@ export class Notification {
     return this.props.category;
   }
 
-  public set readAt(readAt: Date | null | undefined) {
-    this.readAt = readAt;
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
   }
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
   }
 
-  public cancel(){
-this.props.canceledAt = new Date();
+  public cancel() {
+    this.props.canceledAt = new Date();
   }
 
   public get canceledAt(): Date | null | undefined {
