242771e79ad8c30f988a42d77d31d3c05a7389d1
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 09:24:27 -0400

    feat: setup count recipient notifications

--- src/app/use-cases/count-recipient-notification.spec.ts
+++ src/app/use-cases/count-recipient-notification.spec.ts
@@ -1,29 +1,30 @@
-import { Content } from '@app/entities/content';
-import { Notification } from '@app/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
-import { CancelNotification } from './cancel-notification';
-import { NotificationNotFound } from './errors/notification-not-found';
+import { CountRecipientNotifications } from './count-recipient-notifications';
 
 describe('Count recipient notifications', () => {
-  it('should be able to cancel a notification', async () => {
+  it('should be able to count recipient notifications', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
-    const cancelNotification = new CancelNotification(notificationsRepository);
-
-    const notification = new Notification({
-      category: 'social',
-      content: new Content('Nova solicitação de amizade'),
-      recipientId: 'example',
-    });
-
-    await notificationsRepository.create(notification);
-    await cancelNotification.execute({
-      notificationId: notification.id,
-    });
-
-    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
-      expect.any(Date),
+    const countRecipientNotifications = new CountRecipientNotifications(
+      notificationsRepository,
     );
-  });
 
-  
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(count).toEqual(2);
+  });
 });
