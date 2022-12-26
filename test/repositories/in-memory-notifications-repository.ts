33880d1fe2dba23d2968fd77a51f9bd13f294386
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 08:45:57 -0400

    feat(in-memory-repository: implement methods save and findById

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,11 +1,21 @@
 import { Notification } from '@app/entities/notification';
 import { NotificationsRepository } from '@app/repositories/notifications-repository';
 
-export class InMemoryNotificationsRepository implements NotificationsRepository {
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
   public notifications: Notification[] = [];
 
   async findById(notificationId: string): Promise<Notification> {
-    throw new Error('Method not implemented.');
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification
   }
 
   async create(notification: Notification) {
@@ -13,6 +23,6 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
   }
 
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const notificationIndex = this.notifications.findIndex((item) => item.id === notification.id)
   }
 }
