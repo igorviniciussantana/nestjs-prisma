d64de267060e77d5f3768afbc01f75e3422a1e6d
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 10:54:15 -0400

    feat(notification controller): add cancel route

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,32 @@
-import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { Body, Controller, Get, Post, Patch, Request } from '@nestjs/common';
 import { SendNotification } from '@app/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { NotificationViewModel } from '../view-models/notification-view-model';
+import { Param } from '@nestjs/common';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
+
+  async countFromRecipient() {}
+
+  async getFromRecipient() {}
+
+  async read() {}
+
+  async unread() {}
+
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     const { category, content, recipientId } = body;
@@ -16,6 +37,6 @@ export class NotificationsController {
       content,
     });
 
-    return { notification: NotificationViewModel.toHTTP(notification) }
+    return { notification: NotificationViewModel.toHTTP(notification) };
   }
 }
