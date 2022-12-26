6c99d0971cfcca2f9acc7215a4096251a0fff070
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 08:27:01 -0400

    test: create cancel-notification test

---
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,26 @@
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade'),
+      recipientId: 'example',
+    });
+
+    await notificationsRepository.create(notification);
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+});
