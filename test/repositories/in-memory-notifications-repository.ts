0b10b6488584a3cd0e4f91e593c10e3da2278544
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 09:11:22 -0400

    feat: implement countManyByRecipientId method

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -18,11 +18,21 @@ export class InMemoryNotificationsRepository
     return notification
   }
 
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(notification => notification.recipientId === recipientId).length
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
 
   async save(notification: Notification): Promise<void> {
-    const notificationIndex = this.notifications.findIndex((item) => item.id === notification.id)
+    const notificationIndex = this.notifications.findIndex((item) => item.id === notification.id) 
+    
+    if(notificationIndex >= 0){
+      this.notifications[notificationIndex] = notification
+    }
   }
+
+ 
 }
