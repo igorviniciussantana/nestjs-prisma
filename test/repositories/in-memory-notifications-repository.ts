b227db72d2efbea64e5092e78417a84332ba13a4
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 08:16:06 -0400

    fix: fix InMemoryRepositories

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,7 +4,15 @@ import { NotificationsRepository } from '@app/repositories/notifications-reposit
 export class InMemoryNotificationsRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
