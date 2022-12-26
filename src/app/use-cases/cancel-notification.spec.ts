97944dbe950a98c58a2ac6ec3f415492973cb45e
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-26 08:54:54 -0400

    test: add non existing notification error test

--- src/app/use-cases/cancel-notification.spec.ts
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -2,6 +2,7 @@ import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancel notification', () => {
   it('should be able to cancel a notification', async () => {
@@ -23,4 +24,16 @@ describe('Cancel notification', () => {
       expect.any(Date),
     );
   });
+
+  it('should not be able to cancel a non existing notification', async ()=>{
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+
+      expect(() => {
+        return cancelNotification.execute({
+            notificationId: 'fake-notification-id',
+          });
+      }).rejects.toThrow(NotificationNotFound)
+  })
 });
